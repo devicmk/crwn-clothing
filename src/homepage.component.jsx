c18bb8e91ea696363a8a7761843e4889aa0e30e3
Marko Devic <devicmk@Markos-MacBook-Pro.local>
2020-12-24 22:32:36 +0100

    making homepage componenent and placed first sass file

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import './homepage.styles.scss';
 
 const HomePage = () => (
-    <div>
     <div className='homepage'>
         <div className='directory-menu'>
             <div className='menu-item'> 
@@ -12,40 +11,24 @@ const HomePage = () => (
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
-        </div>
-    </div>
-    <div className='homepage'>
-        <div className='directory-menu'>
             <div className='menu-item'> 
                 <div className='content'>
                     <h1 className='title'>JACKETS</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
-        </div>
-    </div>
-    <div className='homepage'>
-        <div className='directory-menu'>
             <div className='menu-item'> 
                 <div className='content'>
                     <h1 className='title'>SNEAKERS</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
-        </div>
-    </div>
-    <div className='homepage'>
-        <div className='directory-menu'>
             <div className='menu-item'> 
                 <div className='content'>
                     <h1 className='title'>WOMENS</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
-        </div>
-    </div>
-    <div className='homepage'>
-        <div className='directory-menu'>
             <div className='menu-item'> 
                 <div className='content'>
                     <h1 className='title'>MENS</h1>
@@ -54,7 +37,7 @@ const HomePage = () => (
             </div>
         </div>
     </div>
-    </div>
+        
 )
 
 export default HomePage;
